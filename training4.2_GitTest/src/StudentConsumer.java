de8989eb78d9b2c5ccb8055aac8c43876b922ca8
Florin <Florin@FAM>
2017-07-20 15:54:29 +0300

    my first commit

---
+++ training4.2_GitTest/src/StudentConsumer.java
@@ -0,0 +1,14 @@
+
+public class StudentConsumer {
+	
+	public static void main(String[] args)
+	{
+		
+		Student s1 = new Student();
+		s1.setName("Ion");
+		s1.setAge(18);
+		
+		System.out.println("Hello " + s1.getName());
+	}
+
+}
